f0908bbe06bcdfb1a273d6e0c816e62ec8fa36ee
EasyZhang <35111939+skyisgreen@users.noreply.github.com>
2018-10-19 00:36:50 +0800

    煤球数目
    煤球数目

---
+++ test001/src/test001.java
@@ -0,0 +1,18 @@
+
+public class test001 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//错位，看懂题目很简单
+		int index=0;
+		int sum=0;
+		for(int i=1; i<=100;i++)
+		{
+			index+=i;
+			sum+=index;
+			System.out.println(sum);
+		}
+
+	}
+
+}
