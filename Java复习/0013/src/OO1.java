ab7957a370e23be6561e1871d078006c27613792
EasyZhang <35111939+skyisgreen@users.noreply.github.com>
2018-12-26 22:12:24 +0800

    封装

---
+++ Java复习/0013/src/OO1.java
@@ -0,0 +1,22 @@
+public class OO1 {
+
+    private String name;
+    private int gender;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGender() {
+        return gender == 0 ? "man" : "woman";
+    }
+
+    public void work() {
+        if (18 <= age && age <= 50) {
+            System.out.println(name + " is working very hard!");
+        } else {
+            System.out.println(name + " can't work any more!");
+        }
+    }
+}
